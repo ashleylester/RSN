2f7bca5a4e5ffb0ad1742d571414caacfa047c4c
Kirill Savine <kirill.savine@gmail.com>
2018-06-06 16:25:11 -0400

    Update rsn2.r

--- rsn2.r
+++ rsn2.r
@@ -49,9 +49,9 @@ rsn_table_api=function(
 	inital_check=FALSE,
 	...	
 ){
-	### ServiceNow Table API
-	### COPYRIGHT HOLDER: Kirill Savine 
+	### ServiceNow API R interface
 	### YEAR: 2016
+	### Author: Kirill Savine 	
 	### original source: https://github.com/kirillsavine/RSN
 	
 	st=Sys.time()	
@@ -155,9 +155,10 @@ rsn_aggr_api=function(
 	retry=TRUE,	
 	...	
 ){
-	### ServiceNow Aggregate API
-	### COPYRIGHT HOLDER: Kirill Savine 
+	### ServiceNow API R interface
 	### YEAR: 2016
+	### Author: Kirill Savine 	
+	### original source: https://github.com/kirillsavine/RSN
 	
 	st=Sys.time()	
 	options(scipen = 999)	
